60218aac85047ae84e3877cffa7c13b60ee6a8c8
Brad Lange <blange@gulllakecs.org>
2019-12-29 16:05:41 -0500

    Add Java Code - See Comments in Code
    I had to do some research for it wouldn't work but feel amazing that I figured it out with some help from my friends on Stack Overflow.
    
    Test on Nexus 5X and is stable

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -1,14 +1,45 @@
+/**
+ * IMPORTANT: Make sure you are using the correct package name.
+ * This example uses the package name:
+ * package com.example.android.justjava
+ * If you get an error when copying this code into Android studio, update it to match teh package name found
+ * in the project's AndroidManifest.xml file.
+ **/
+
+// ********MR. LANGE - I went to the AndroidManifest.xml document as indicated within the comments
+// above and changed to represent what I have below.  File path = app/src/main*********
 package com.example.justjava;
 
-import androidx.appcompat.app.AppCompatActivity;
+
 
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;  //MR. LANGE - Upon research via StackOverflow @ https://stackoverflow.com/questions/23330816/error-package-android-support-v7-app-does-not-exist
+import android.view.View;
+import android.widget.TextView;
 
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(1);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
     }
 }

71281312d9331fb483ebd185b71bb7c6d8217609
Brad Lange <blange@gulllakecs.org>
2019-12-29 17:40:49 -0500

    Add Text Views .XML for $ and updated Java with Auto Import turned on
    ****MAKE SURE you turn on Auto Import**** Tested on Nexus 5X and is Stable

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -11,11 +11,14 @@
 package com.example.justjava;
 
 
-
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;  //MR. LANGE - Upon research via StackOverflow @ https://stackoverflow.com/questions/23330816/error-package-android-support-v7-app-does-not-exist
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.NumberFormat;
+
 
 /**
  * This app displays an order form to order coffee.
@@ -41,5 +44,13 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
